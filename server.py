7a324b7a4e9fda6a32616a8cf228799952cba8ed
Puneeth0106 <puneethkumaramudala7@gmail.com>
2026-01-11 08:01:00 -0500

    Changed the transport to sse

--- server.py
+++ server.py
@@ -177,4 +177,4 @@ def categories():
         return f.read()
 
 if __name__ == "__main__":
-    mcp.run(transport="http", host="0.0.0.0", port=8000)
+    mcp.run(transport="sse", host="0.0.0.0", port=8080)
